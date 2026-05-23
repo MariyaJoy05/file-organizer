61ba5eaaadd52f054396e76f3d46848313a87c7f
MariyaJoy05 <mariyajoy1919@gmail.com.com>
2026-05-23 15:51:37 +0530

    Step 2 - detect file extensions and categories

--- organizer.py
+++ organizer.py
@@ -6,4 +6,26 @@ files = os.listdir(folder)
 print("Files found in floder:")
 for file in files:
     print(file)
-    
+
+print("\nFiles with their types:")
+for file in files:
+    name,extension = os.path.splitext(file)
+    print(file,"->",extension)    
+
+print("\nCategory of each file:")
+for file in files:
+    name,extension = os.path.splitext(file)
+
+    if extension in [".jpg" , ".jpeg" , ".png" , ".gif"]:
+        category = "Images"
+    elif extension in [".pdf",".docx",".txt"]:
+        category = "Documents"
+    elif extension in [".mp3",".wav"]:
+        category = "Music"
+    elif extension in [".mp4" , ".mov"]:
+        category = "Videos"
+    else:
+        category = "Others"
+
+    print(file,"->",category)     
+
